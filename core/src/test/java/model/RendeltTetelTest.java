29049cbe174b4499dec69b3dee07850c332a4306
VajdaBea <57557354+VajdaBea@users.noreply.github.com>
2020-02-02 17:53:41 +0100

    Fixed RendeltTetel test.

--- core/src/test/java/model/RendeltTetelTest.java
+++ core/src/test/java/model/RendeltTetelTest.java
@@ -10,23 +10,28 @@ public class RendeltTetelTest {
     Pizza pizza;
     Feltet feltet1;
     Feltet feltet2;
+    RendeltTetel rendeltTetel;
 
-@Before
-    public void SetUp(){
-    pizza = new Pizza("pizza15", PizzaMeret.nagy_45, 2000);
-    feltet1 = new Feltet("paradicsom", 150);
-    feltet2 = new Feltet("kukorica", 50);
-}
+    @Before
+    public void SetUp() {
+        pizza = new Pizza("pizza15", PizzaMeret.nagy_45, 2000);
+        feltet1 = new Feltet("paradicsom", 150);
+        feltet2 = new Feltet("kukorica", 50);
+        rendeltTetel = new RendeltTetel(1, pizza, feltet1, feltet2);
+    }
 
-@Test
-    public void getPizzaTest(){
-    assertEquals();
+    @Test
+    public void getPizzaTest() {
+        assertEquals(pizza, rendeltTetel.getPizza());
+    }
 
+    @Test
+    public void getFeltet1Test() {
+        assertEquals(feltet1, rendeltTetel.getFeltet1());
+    }
 
-}
-@Test
-    public void getFeltetTest(){
-
-}
-
+    @Test
+    public void getFeltet2Test() {
+        assertEquals(feltet2, rendeltTetel.getFeltet2());
+    }
 }
